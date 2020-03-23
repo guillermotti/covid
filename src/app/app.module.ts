7222adabf1f8506d53a3f9014777715778bbb456
Guille Vigil <guillermotti@Guilles-MacBook-Pro.local>
2020-03-23 10:23:50 +0100

    Minor fix on language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,7 +30,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 }
 
 export function BrowserLanguage() {
-  return navigator.language;
+  return navigator.language.split('-')[0];
 }
 
 @NgModule({
