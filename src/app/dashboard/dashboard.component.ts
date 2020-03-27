5623114e63d3e1b973cd0ec45f516933654dbb1f
Alex Albuerne <aalbuerneare@gmail.com>
2020-03-27 08:29:22 +0100

    Styles for desktop and mobile

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -31,8 +31,9 @@ export class DashboardComponent implements OnInit {
     domain: ['#40486a', '#4fd0f9', '#8f99af', '#95a7f6', "#6e7cb6", "#485177", "#313751", "#3b9bb9", "#276579", "#4fd0f9", "#12303a", "5b616f", "cedcfb", "8f99af", "aab0b4", "99acbc"]
   }
   chartNumberItems: any[] = [];
-  selected: number = 5;
+  selected: number = 10;
   chartType: string = 'cases';
+  responsiveType = '';
 
   displayedColumns: string[] = ['country', 'confirmed', 'critical', 'deceased', 'active', 'recovered'];
   dataSource: MatTableDataSource<any>;
@@ -121,6 +122,22 @@ export class DashboardComponent implements OnInit {
       this.snackBar.open(data.name, data.value, {
         duration: 4000,
       });
+    }
+  }
+
+  ngDoCheck() {
+    if (window.screen.width < 599) { // 768px portrait
+      this.responsiveType = 'mobile';
+    } else if (window.screen.width >= 599 && window.screen.width < 960) {
+      this.responsiveType = 'tablet-sm';
+    } else if (window.screen.width >= 960 && window.screen.width < 1024) {
+      this.responsiveType = 'tablet';
+    } else if (window.screen.width >= 1024 && window.screen.width < 1280) {
+      this.responsiveType = 'desktop-sm';
+    } else if (window.screen.width >= 1280 && window.screen.width <= 1920) {
+      this.responsiveType = 'desktop';
+    } else {
+      this.responsiveType = 'large-screen';
     }
   }
 
