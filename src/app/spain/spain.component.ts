60af4d14813af60f8e1e7b7c0dfacf0f254d4c9c
Guille Vigil <guillermotti@Guilles-MacBook-Pro.local>
2020-03-30 12:09:26 +0200

    Graphs for Spain

--- src/app/spain/spain.component.ts
+++ src/app/spain/spain.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
 import { HttpClient } from '@angular/common/http';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-spain',
@@ -31,13 +32,44 @@ export class SpainComponent implements OnInit {
     "PV": "País Vasco",
     "RI": "La Rioja"
   }
-
   today: Date = new Date();
-
   displayedColumns: string[] = ['date', 'comunidad', 'cases', 'hospital', 'critical', 'deceased'];
   dataSource: MatTableDataSource<any>;
+  responsiveType = '';
 
-  constructor(private httpClient: HttpClient, private ngxCsvParser: NgxCsvParser) { }
+  //Charts
+  isTotalSelected: boolean = true;
+  isDeceasedSelected: boolean = false;
+  isCriticalSelected: boolean = false;
+  isActiveSelected: boolean = false;
+  isBarSelected: boolean = true;
+  isGaugeSelected: boolean = false;
+  isPieSelected: boolean = false
+  chart: string = 'bar';
+  colorScheme: any = {
+    domain: ['#40486a', '#4fd0f9', '#8f99af', '#95a7f6', "#6e7cb6", "#485177", "#313751", "#3b9bb9", "#276579", "#4fd0f9", "#12303a", "5b616f", "cedcfb", "8f99af", "aab0b4", "99acbc"]
+  }
+  chartType: string = 'Casos ';
+  spainData: any;
+  barChart = {
+    showXAxis: true,
+    showYAxis: true,
+    showXAxisLabel: true,
+    showDataLabel: true,
+    data: []
+  }
+
+  pieChart = {
+    showLabels: true,
+    isDoughnut: false,
+    data: []
+  }
+
+  gaugeChart = {
+    data: []
+  }
+
+  constructor(private httpClient: HttpClient, private ngxCsvParser: NgxCsvParser, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.httpClient.get('assets/csv/covid.csv', { responseType: 'arraybuffer' }).subscribe(response => {
@@ -46,9 +78,14 @@ export class SpainComponent implements OnInit {
         .pipe().subscribe((result: Array<any>) => {
           result = result.slice(Math.max(result.length - 20, 0))
           result.pop();
+          result.sort((a, b) => { return b['Casos '] - a['Casos '] });
           result.map(item => {
             item["CCAA Codigo ISO"] = this.ccaa[item["CCAA Codigo ISO"]];
           });
+          this.spainData = result;
+          this.setChart(this.barChart, 'CCAA Codigo ISO', this.chartType);
+          this.setChart(this.pieChart, 'CCAA Codigo ISO', this.chartType);
+          this.setChart(this.gaugeChart, 'CCAA Codigo ISO', this.chartType);
           this.dataSource = new MatTableDataSource(result);
           console.log('Result', result);
         }, (error: NgxCSVParserError) => {
@@ -57,9 +94,118 @@ export class SpainComponent implements OnInit {
     });
   }
 
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+    if (navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i)) {
+      this.snackBar.open(data.name, data.value, {
+        duration: 4000,
+      });
+    }
+  }
+
+  ngDoCheck() {
+    if (window.screen.width < 599) { // 768px portrait
+      this.responsiveType = 'mobile';
+    } else if (window.screen.width >= 599 && window.screen.width < 960) {
+      this.responsiveType = 'tablet-sm';
+    } else if (window.screen.width >= 960 && window.screen.width < 1024) {
+      this.responsiveType = 'tablet';
+    } else if (window.screen.width >= 1024 && window.screen.width < 1280) {
+      this.responsiveType = 'desktop-sm';
+    } else if (window.screen.width >= 1280 && window.screen.width <= 1920) {
+      this.responsiveType = 'desktop';
+    } else {
+      this.responsiveType = 'large-screen';
+    }
+  }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  setChart(chart: object, yLabel: string, xLabel: string) {
+    chart['data'] = [];
+    this.spainData.sort((a, b) => { return b[xLabel] - a[xLabel] });
+    this.spainData.map(item => {
+      chart['data'].push({ name: item[yLabel], value: item[xLabel] });
+    })
+    Object.assign(this, chart['data']);
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+
+  select(type: string) {
+    this.chartType = type;
+    switch (type) {
+      case 'Casos ': {
+        this.isTotalSelected = true;
+        this.isDeceasedSelected = false;
+        this.isCriticalSelected = false
+        this.isActiveSelected = false;
+        break;
+      }
+      case 'Fallecidos': {
+        this.isTotalSelected = false;
+        this.isDeceasedSelected = true;
+        this.isCriticalSelected = false
+        this.isActiveSelected = false;
+        break;
+      }
+      case 'UCI': {
+        this.isTotalSelected = false;
+        this.isDeceasedSelected = false;
+        this.isCriticalSelected = true
+        this.isActiveSelected = false;
+        break;
+      }
+      case 'Hospitalizados': {
+        this.isTotalSelected = false;
+        this.isDeceasedSelected = false;
+        this.isCriticalSelected = false
+        this.isActiveSelected = true;
+        break;
+      }
+    }
+    this.setChart(this.barChart, 'CCAA Codigo ISO', this.chartType);
+    this.setChart(this.pieChart, 'CCAA Codigo ISO', this.chartType);
+    this.setChart(this.gaugeChart, 'CCAA Codigo ISO', this.chartType);
+  }
+
+  selectChart(type: string) {
+    this.chart = type;
+    switch (type) {
+      case 'bar': {
+        this.isBarSelected = true;
+        this.isPieSelected = false;
+        this.isGaugeSelected = false;
+        break;
+      }
+      case 'pie': {
+        this.isBarSelected = false;
+        this.isPieSelected = true;
+        this.isGaugeSelected = false;
+        break;
+      }
+      case 'gauge': {
+        this.isBarSelected = false;
+        this.isPieSelected = false;
+        this.isGaugeSelected = true;
+        break;
+      }
+    }
   }
 
 }
